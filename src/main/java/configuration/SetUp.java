a6bf68577aabc60b5621c09a99173e33061ea604
rohitpritamrai <rohitpritamrai@gmail.com>
2021-09-13 21:01:40 +0530

    To load the properties file in BeforeSuite annotation and fetch the application URL from properties file

--- src/main/java/configuration/SetUp.java
+++ src/main/java/configuration/SetUp.java
@@ -6,13 +6,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
 public class SetUp {
+    public static Properties properties;
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     public static void setDriver() {
         WebDriverManager.chromedriver().setup();
         driver.set(new ChromeDriver());
-        getDriver().get("URL");
+        getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        System.out.println("Maximizing Browser Window");
+        getDriver().manage().window().maximize();
+        String application_url = properties.getProperty("URL");
+        getDriver().get(application_url);
+        System.out.println("Navigated to URL "+application_url);
 
     }
 
@@ -24,12 +34,17 @@ public class SetUp {
         driver.get().close();
     }
 
-    @BeforeSuite
+    @BeforeSuite(alwaysRun = true)
     public void setUp_beforeSuite() {
+        try {
+            properties = PropertiesFileLoad.LoadPropertiesFile("src/test/java/resources/properties/Env.properties");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         setDriver();
     }
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public void tearDown() {
         getDriver().quit();
     }
